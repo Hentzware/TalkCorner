4bb16231d712b2f9132a134c6413e9f8ea146967
Henning Entz <entz.henning@gmail.com>
2025-08-02 01:18:21 +0200

    feat(application): Add IBoardRepository interface for Board entity

---
+++ TalkCorner.Application/Contracts/Persistence/IBoardRepository.cs
@@ -0,0 +1,10 @@
+﻿using TalkCorner.Domain.Entities;
+
+namespace TalkCorner.Application.Contracts.Persistence;
+
+/// <summary>
+///     Repository interface for <see cref="Board" /> entities.
+/// </summary>
+public interface IBoardRepository : IGenericRepository<Board>
+{
+}

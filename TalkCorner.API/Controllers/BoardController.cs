6608b33604980637468b1c05806403d660621e73
Henning Entz <entz.henning@gmail.com>
2025-08-02 01:15:13 +0200

    Add BoardController with endpoint to get all boards

---
+++ TalkCorner.API/Controllers/BoardController.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TalkCorner.Application.Features.Board.GetAllBoardsQuery;
+
+namespace TalkCorner.API.Controllers;
+
+[Route("api/boards")]
+[ApiController]
+public class BoardController(IMediator mediator) : ControllerBase
+{
+    public async Task<ActionResult<IEnumerable<GetAllBoardsDto>>> GetAsync()
+    {
+        var request = new GetAllBoardsQuery();
+        var response = await mediator.Send(request);
+        return Ok(response);
+    }
+}

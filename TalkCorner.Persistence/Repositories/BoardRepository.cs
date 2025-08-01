dc211aac2f68ecd8fdbb02efd7c7c354c02a9386
Henning Entz <entz.henning@gmail.com>
2025-08-02 01:29:58 +0200

    feat(persistence): Implement BoardRepository inheriting from GenericRepository

---
+++ TalkCorner.Persistence/Repositories/BoardRepository.cs
@@ -0,0 +1,12 @@
+﻿using TalkCorner.Application.Contracts.Persistence;
+using TalkCorner.Domain.Entities;
+using TalkCorner.Persistence.DatabaseContext;
+
+namespace TalkCorner.Persistence.Repositories;
+
+/// <summary>
+///     EF Core implementation of the board repository.
+/// </summary>
+public class BoardRepository(TalkCornerDbContext context) : GenericRepository<Board>(context), IBoardRepository
+{
+}

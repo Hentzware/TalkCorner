07474b711c0b9deec7bf30fe1f53e9131b654253
Henning Entz <entz.henning@gmail.com>
2025-08-02 01:19:57 +0200

    feat(application): Add GetAllBoardsDto for board list endpoint

---
+++ TalkCorner.Application/Features/Board/GetAllBoardsQuery/GetAllBoardsDto.cs
@@ -0,0 +1,20 @@
+﻿namespace TalkCorner.Application.Features.Board.GetAllBoardsQuery;
+
+public class GetAllBoardsDto
+{
+    public Guid Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public int ThreadCount { get; set; }
+
+    public int SubBoardCount { get; set; }
+
+    public Guid? ParentBoardId { get; set; }
+
+    public DateTime? Created { get; set; }
+
+    public DateTime? Updated { get; set; }
+}

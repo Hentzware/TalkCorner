9e4bd2750a50ec3752011fdb8f591b0c8a7bf791
Henning Entz <entz.henning@gmail.com>
2025-08-02 01:29:32 +0200

    chore(persistence): Add design-time DbContext factory for TalkCornerDbContext

---
+++ TalkCorner.Persistence/DatabaseContext/TalkCornerDbContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace TalkCorner.Persistence.DatabaseContext;
+
+public class TalkCornerDbContextFactory : IDesignTimeDbContextFactory<TalkCornerDbContext>
+{
+    public TalkCornerDbContext CreateDbContext(string[] args)
+    {
+        var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.Development.json", false)
+            .Build();
+
+        var optionsBuilder = new DbContextOptionsBuilder<TalkCornerDbContext>();
+        var connectionString = config.GetConnectionString("TalkCornerDb");
+
+        Console.WriteLine(connectionString);
+
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            throw new InvalidOperationException("ConnectionString TalkCornerDb not found.");
+        }
+
+        optionsBuilder.UseMySql(connectionString, new MariaDbServerVersion(new Version(9, 4, 0)));
+
+        return new TalkCornerDbContext(optionsBuilder.Options);
+    }
+}

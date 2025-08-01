e467920010c5271a34d1237d576dff9cfa46cd01
Henning Entz <entz.henning@gmail.com>
2025-08-02 01:23:16 +0200

    feat(domain): Add abstract ValueObject base class with equality logic

---
+++ TalkCorner.Domain/Common/ValueObject.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TalkCorner.Domain.Common;
+
+[Owned]
+public abstract class ValueObject
+{
+    protected abstract IEnumerable<object> GetEqualityComponents();
+
+    public override bool Equals(object? obj)
+    {
+        return obj is ValueObject other && Equals(other);
+    }
+
+    public bool Equals(ValueObject? other)
+    {
+        return other != null &&
+               GetEqualityComponents().SequenceEqual(other.GetEqualityComponents());
+    }
+
+    public override int GetHashCode()
+    {
+        return GetEqualityComponents()
+            .Aggregate(0, (hash, component) =>
+                HashCode.Combine(hash, component?.GetHashCode() ?? 0));
+    }
+}

8305d5137f8314dbc113ef1fa8fdab62c47636df
Henning Entz <entz.henning@gmail.com>
2025-08-02 01:21:48 +0200

    chore(application): Add ApplicationServiceRegistration for MediatR and AutoMapper setup

---
+++ TalkCorner.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TalkCorner.Application;
+
+public static class ApplicationServiceRegistration
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(typeof(ApplicationServiceRegistration).Assembly);
+        });
+
+        services.AddAutoMapper(config =>
+        {
+            config.AddMaps(typeof(ApplicationServiceRegistration).Assembly);
+        });
+
+        return services;
+    }
+}

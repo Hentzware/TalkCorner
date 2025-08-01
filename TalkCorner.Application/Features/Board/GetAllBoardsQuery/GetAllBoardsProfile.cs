26884c68f77b684d51943f38432e846e1bcdc2cb
Henning Entz <entz.henning@gmail.com>
2025-08-02 01:20:25 +0200

    feat(application): Add AutoMapper profile for mapping Board to GetAllBoardsDto

---
+++ TalkCorner.Application/Features/Board/GetAllBoardsQuery/GetAllBoardsProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+
+namespace TalkCorner.Application.Features.Board.GetAllBoardsQuery;
+
+public class GetAllBoardsProfile : Profile
+{
+    public GetAllBoardsProfile()
+    {
+        CreateMap<Domain.Entities.Board, GetAllBoardsDto>()
+            .ForMember(dest => dest.ThreadCount, opt => opt.MapFrom(src => src.Threads.Count))
+            .ForMember(dest => dest.SubBoardCount, opt => opt.MapFrom(src => src.SubBoards.Count));
+
+    }
+}

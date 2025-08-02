160e402a860f6ba74580d31b34a87afa654376cf
Henning Entz <entz.henning@gmail.com>
2025-08-03 01:47:34 +0200

    feat: Add DeleteBoard

---
+++ TalkCorner.Application/Features/Board/DeleteBoard/DeleteBoardCommand.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+using MediatR;
+
+namespace TalkCorner.Application.Features.Board.DeleteBoard;
+
+public class DeleteBoardCommand : IRequest<Unit>
+{
+    [JsonIgnore]
+    public Guid Id { get; init; }
+}
---
+++ TalkCorner.Application/Features/Board/DeleteBoard/DeleteBoardCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using MediatR;
+using TalkCorner.Application.Contracts.Persistence;
+
+namespace TalkCorner.Application.Features.Board.DeleteBoard;
+
+public class DeleteBoardCommandHandler(IBoardRepository boardRepository, IMapper mapper) : IRequestHandler<DeleteBoardCommand, Unit>
+{
+    public async Task<Unit> Handle(DeleteBoardCommand request, CancellationToken cancellationToken)
+    {
+        var board = await boardRepository.GetBoardByIdWithTrackingAsync(request.Id);
+
+        if (board == null)
+        {
+            throw new InvalidOperationException("Board does not exist.");
+        }
+
+        await boardRepository.DeleteAsync(board, cancellationToken);
+        await boardRepository.UnitOfWork.SaveChangesAsync(cancellationToken);
+
+        return Unit.Value;
+    }
+}

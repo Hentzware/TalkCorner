f0d082217859038411ec302b934ad7e280023fe7
Henning Entz <entz.henning@gmail.com>
2025-08-03 01:46:24 +0200

    feat: Add CreateBoard

---
+++ TalkCorner.Application/Features/Board/CreateBoard/CreateBoardCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace TalkCorner.Application.Features.Board.CreateBoard;
+
+public class CreateBoardCommand : IRequest<Unit>
+{
+    public Guid CreatedByUserId { get; set; }
+
+    public Guid? ParentBoardId { get; set; }
+
+    public string Description { get; set; } = string.Empty;
+
+    public string Title { get; set; } = string.Empty;
+}
---
+++ TalkCorner.Application/Features/Board/CreateBoard/CreateBoardCommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using MediatR;
+using TalkCorner.Application.Contracts.Persistence;
+
+namespace TalkCorner.Application.Features.Board.CreateBoard;
+
+public class CreateBoardCommandHandler(IBoardRepository boardRepository, IMapper mapper) : IRequestHandler<CreateBoardCommand, Unit>
+{
+    public async Task<Unit> Handle(CreateBoardCommand request, CancellationToken cancellationToken)
+    {
+        var board = mapper.Map<Domain.Entities.Board>(request);
+
+        await boardRepository.AddAsync(board, cancellationToken);
+        await boardRepository.UnitOfWork.SaveChangesAsync(cancellationToken);
+
+        return Unit.Value;
+    }
+}

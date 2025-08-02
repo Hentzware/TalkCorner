876ec53d4573da2b7c23353144cc250559986488
Henning Entz <entz.henning@gmail.com>
2025-08-03 01:56:12 +0200

    feat: Add GET, PUT, DELETE, POST.

--- TalkCorner.API/Controllers/BoardController.cs
+++ TalkCorner.API/Controllers/BoardController.cs
@@ -1,6 +1,10 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using TalkCorner.Application.Features.Board.GetAllBoardsQuery;
+using TalkCorner.Application.Features.Board.CreateBoard;
+using TalkCorner.Application.Features.Board.DeleteBoard;
+using TalkCorner.Application.Features.Board.GetAllBoards;
+using TalkCorner.Application.Features.Board.GetBoardById;
+using TalkCorner.Application.Features.Board.UpdateBoard;
 
 namespace TalkCorner.API.Controllers;
 
@@ -8,10 +12,42 @@ namespace TalkCorner.API.Controllers;
 [ApiController]
 public class BoardController(IMediator mediator) : ControllerBase
 {
+    [HttpGet]
     public async Task<ActionResult<IEnumerable<GetAllBoardsDto>>> GetAsync()
     {
         var request = new GetAllBoardsQuery();
         var response = await mediator.Send(request);
         return Ok(response);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<GetBoardByIdDto>> GetByIdAsync(Guid id)
+    {
+        var request = new GetBoardByIdQuery(id);
+        var response = await mediator.Send(request);
+        return Ok(response);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> CreateBoardAsync([FromBody] CreateBoardCommand request)
+    {
+        await mediator.Send(request);
+        return Ok();
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<ActionResult> DeleteBoardAsync(Guid id)
+    {
+        var request = new DeleteBoardCommand() { Id = id };
+        await mediator.Send(request);
+        return Ok();
+    }
+
+    [HttpPut("{id:guid}")]
+    public async Task<ActionResult> UpdateBoardAsync(Guid id, [FromBody] UpdateBoardCommand request)
+    {
+        request.Id = id;
+        await mediator.Send(request);
+        return Ok();
     }
 }

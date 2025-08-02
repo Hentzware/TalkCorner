24c2f2c6ada1b530ef6571c97ed27bc6ce1d674c
Henning Entz <entz.henning@gmail.com>
2025-08-03 01:48:03 +0200

    feat: Add GetAllBoards

---
+++ TalkCorner.Application/Features/Board/GetAllBoards/GetAllBoardsQuery.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace TalkCorner.Application.Features.Board.GetAllBoards;
+
+public record GetAllBoardsQuery : IRequest<IEnumerable<GetAllBoardsDto>>;
---
+++ TalkCorner.Application/Features/Board/GetAllBoards/GetAllBoardsQueryHandler.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using MediatR;
+using TalkCorner.Application.Contracts.Persistence;
+
+namespace TalkCorner.Application.Features.Board.GetAllBoards;
+
+public class GetAllBoardsQueryHandler(IBoardRepository boardRepository, IMapper mapper) : IRequestHandler<GetAllBoardsQuery, IEnumerable<GetAllBoardsDto>>
+{
+    public async Task<IEnumerable<GetAllBoardsDto>> Handle(GetAllBoardsQuery request, CancellationToken cancellationToken)
+    {
+        var boards = await boardRepository.GetBoardsAsync();
+        var response = mapper.Map<IEnumerable<GetAllBoardsDto>>(boards);
+        return response;
+    }
+}

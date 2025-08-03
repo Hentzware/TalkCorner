0566a149f312b514084e85f9c066e588b74d8370
Henning Entz <entz.henning@gmail.com>
2025-08-03 14:15:01 +0200

    feat: Implemented DeletePost

--- TalkCorner.Application/Features/Post/DeletePost/DeletePostCommand.cs
+++ TalkCorner.Application/Features/Post/DeletePost/DeletePostCommand.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.Json.Serialization;
+using MediatR;
 
-namespace TalkCorner.Application.Features.Post.DeletePost
+namespace TalkCorner.Application.Features.Post.DeletePost;
+
+public class DeletePostCommand : IRequest<Unit>
 {
-    internal class DeletePostCommand
-    {
-    }
+    [JsonIgnore]
+    public Guid Id { get; init; }
 }
--- TalkCorner.Application/Features/Post/DeletePost/DeletePostCommandHandler.cs
+++ TalkCorner.Application/Features/Post/DeletePost/DeletePostCommandHandler.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using MediatR;
+using TalkCorner.Application.Contracts.Persistence;
 
-namespace TalkCorner.Application.Features.Post.DeletePost
+namespace TalkCorner.Application.Features.Post.DeletePost;
+
+public class DeletePostCommandHandler(IPostRepository postRepository, IMapper mapper) : IRequestHandler<DeletePostCommand, Unit>
 {
-    internal class DeletePostCommandHandler
+    public async Task<Unit> Handle(DeletePostCommand request, CancellationToken cancellationToken)
     {
+        var post = await postRepository.GetPostByIdWithTrackingAsync(request.Id);
+
+        if (post == null)
+        {
+            throw new InvalidOperationException("Post does not exist.");
+        }
+
+        await postRepository.DeleteAsync(post, cancellationToken);
+        await postRepository.UnitOfWork.SaveChangesAsync(cancellationToken);
+
+        return Unit.Value;
     }
 }

2f36b84a159727b9e2d766ec5b4e5e965bcc3bc4
Henning Entz <entz.henning@gmail.com>
2025-08-03 13:52:35 +0200

    style: Code Cleanup

--- TalkCorner.Application/Features/Board/GetAllBoards/GetAllBoardsDto.cs
+++ TalkCorner.Application/Features/Board/GetAllBoards/GetAllBoardsDto.cs
@@ -2,19 +2,19 @@
 
 public class GetAllBoardsDto
 {
-    public Guid Id { get; set; }
-
-    public string Title { get; set; } = null!;
-
-    public string Description { get; set; } = null!;
-
-    public int ThreadCount { get; set; }
-
-    public int SubBoardCount { get; set; }
-
-    public Guid? ParentBoardId { get; set; }
-
     public DateTime? Created { get; set; }
 
     public DateTime? Updated { get; set; }
+
+    public Guid Id { get; set; }
+
+    public Guid? ParentBoardId { get; set; }
+
+    public int SubBoardCount { get; set; }
+
+    public int ThreadCount { get; set; }
+
+    public string Description { get; set; } = null!;
+
+    public string Title { get; set; } = null!;
 }
--- TalkCorner.Application/Features/Board/GetAllBoards/GetAllBoardsProfile.cs
+++ TalkCorner.Application/Features/Board/GetAllBoards/GetAllBoardsProfile.cs
@@ -9,6 +9,5 @@ public class GetAllBoardsProfile : Profile
         CreateMap<Domain.Entities.Board, GetAllBoardsDto>()
             .ForMember(dest => dest.ThreadCount, opt => opt.MapFrom(src => src.Threads.Count))
             .ForMember(dest => dest.SubBoardCount, opt => opt.MapFrom(src => src.SubBoards.Count));
-
     }
 }

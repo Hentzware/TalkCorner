b784de532d665cf6a141f37c59cf2fc24884204c
Henning Entz <entz.henning@gmail.com>
2025-08-15 13:07:16 +0200

    feat(login): add demo user and admin login buttons for easier access
    This change introduces two buttons in the login component that allow users
    to quickly log in with demo credentials. This improves user experience
    by providing a straightforward way to access the application without
    needing to create an account.
    
    Additionally, the login logic is implemented to handle actual login
    requests using the provided credentials, enhancing the functionality
    of the login component.

--- TalkCorner.Angular/src/app/pages/login/login.component.ts
+++ TalkCorner.Angular/src/app/pages/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {ApiClient, LoginCommand} from "../../api/board-client";
+import {Router} from "@angular/router";
+import {AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'app-login',
@@ -9,11 +12,31 @@ export class LoginComponent {
   form: FormGroup;
   error: string | null = null;
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private api: ApiClient,
+    private router: Router,
+    private authService: AuthService) {
     this.form = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
     });
+  }
+
+  setDemoUserCredentials(): void {
+    this.form.setValue({
+      email: 'user@localhost.de',
+      password: 'Passw0rd'
+    });
+    this.error = null;
+  }
+
+  setDemoAdminCredentials(): void {
+    this.form.setValue({
+      email: 'admin@localhost.de',
+      password: 'Passw0rd'
+    });
+    this.error = null;
   }
 
   onSubmit() {
@@ -21,8 +44,23 @@ export class LoginComponent {
       this.form.markAllAsTouched();
       return;
     }
-    // TODO: Implement actual login logic (call API etc.)
-    // Beispiel-Fehler:
-    this.error = 'Login ist momentan nur als Demo verfügbar.';
+
+    let loginCommand = new LoginCommand();
+    loginCommand.email = this.form.controls['email'].value;
+    loginCommand.password = this.form.controls['password'].value;
+
+    this.api.login(loginCommand).subscribe({
+      next: (response) => {
+        if (response.token != null) {
+          this.authService.login(response.token);
+          this.router.navigate(['/boards']);
+        } else {
+          this.error = "Kein Token erhalten.";
+        }
+      },
+      error: (err) => {
+        this.error = typeof err === 'string' ? err : (err?.title ?? "Login fehlgeschlagen.");
+      }
+    });
   }
 }

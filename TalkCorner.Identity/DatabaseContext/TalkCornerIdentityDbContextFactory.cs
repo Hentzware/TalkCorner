63ca92e983564ab561c821573c7c72abf2b3dd40
Henning Entz <entz.henning@gmail.com>
2025-08-15 10:41:55 +0200

    feat(TalkCornerIdentity): add TalkCornerIdentityDbContextFactory for design-time DbContext creation and configuration management

---
+++ TalkCorner.Identity/DatabaseContext/TalkCornerIdentityDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace TalkCorner.Identity.DatabaseContext;
+
+public class TalkCornerIdentityDbContextFactory : IDesignTimeDbContextFactory<TalkCornerIdentityDbContext>
+{
+    public TalkCornerIdentityDbContext CreateDbContext(string[] args)
+    {
+        var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.Development.json", false)
+            .Build();
+
+        var optionsBuilder = new DbContextOptionsBuilder<TalkCornerIdentityDbContext>();
+        var connectionString = config.GetConnectionString("TalkCornerIdentityDb");
+
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            throw new InvalidOperationException("ConnectionString TalkCornerIdentityDb not found.");
+        }
+
+        optionsBuilder.UseMySql(connectionString, new MySqlServerVersion(new Version(9, 4, 0)));
+
+        return new TalkCornerIdentityDbContext(optionsBuilder.Options);
+    }
+}

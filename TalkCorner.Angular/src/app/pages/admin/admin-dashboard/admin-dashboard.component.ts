b186854b80e41e3bbedc3fc7bb7193624914fb54
Henning Entz <entz.henning@gmail.com>
2025-08-15 13:05:41 +0200

    feat(admin-dashboard): create admin dashboard component with role-based access control
    This commit introduces a new admin dashboard component that allows
    administrators to manage boards and users. It includes a role-based
    access control mechanism to restrict access to authorized users only.
    The layout is designed for clarity and ease of use, with placeholders
    for future features.

---
+++ TalkCorner.Angular/src/app/pages/admin/admin-dashboard/admin-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {AuthService} from "../../../services/auth.service";
+
+@Component({
+  selector: 'app-admin-dashboard',
+  templateUrl: './admin-dashboard.component.html',
+  styleUrl: './admin-dashboard.component.css'
+})
+export class AdminDashboardComponent {
+constructor(public authService: AuthService) {
+}
+}

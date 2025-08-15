22414413311606e4e6828083321797b260039bf6
Henning Entz <entz.henning@gmail.com>
2025-08-15 10:42:49 +0200

    feat(configurations): add UserConfiguration and UserRoleConfiguration for seeding initial users and roles chore(csproj): add Migrations folder to project file for better organization of migration files

---
+++ TalkCorner.Identity/Configurations/UserConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TalkCorner.Identity.Models;
+
+namespace TalkCorner.Identity.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<ApplicationUser>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+        var passwordHasher = new PasswordHasher<ApplicationUser>();
+
+        builder.HasData(
+            new ApplicationUser()
+            {
+                Id = "C99AC715-51B2-400E-9625-290D19418B70",
+                Email = "admin@localhost.de",
+                NormalizedEmail = "ADMIN@LOCALHOST.DE",
+                UserName = "admin@localhost.de",
+                NormalizedUserName = "ADMIN@LOCALHOST.DE",
+                PasswordHash = passwordHasher.HashPassword(null, "Passw0rd"),
+                EmailConfirmed = true
+            },
+            new ApplicationUser()
+            {
+                Id = "4895AD73-D889-49DE-9418-E8B163BEACB5",
+                Email = "user@localhost.de",
+                NormalizedEmail = "USER@LOCALHOST.DE",
+                UserName = "user@localhost.de",
+                NormalizedUserName = "USER@LOCALHOST.DE",
+                PasswordHash = passwordHasher.HashPassword(null, "Passw0rd"),
+                EmailConfirmed = true
+            }
+        );
+    }
+}
---
+++ TalkCorner.Identity/Configurations/UserRoleConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace TalkCorner.Identity.Configurations;
+
+public class UserRoleConfiguration : IEntityTypeConfiguration<IdentityUserRole<string>>
+{
+    public void Configure(EntityTypeBuilder<IdentityUserRole<string>> builder)
+    {
+        builder.HasData(
+            new IdentityUserRole<string>
+            {
+                UserId = "C99AC715-51B2-400E-9625-290D19418B70",
+                RoleId = "6D82E62C-559F-4489-B6B3-BDA2D3877D41"
+            },
+            new IdentityUserRole<string>
+            {
+                UserId = "4895AD73-D889-49DE-9418-E8B163BEACB5",
+                RoleId = "8102AD91-84CB-423E-8DCA-AD2839D8149D"
+            }
+        );
+    }
+}

e7ba5af9c80d0e36ebeed74575b47545cd3d46d4
Henning Entz <entz.henning@gmail.com>
2025-08-15 10:10:12 +0200

    feat(ThreadController): add new thread management endpoints for closing, opening, sticking, and unsticking threads to enhance functionality refactor(ThreadController): update existing GetThreadById endpoint to improve clarity and maintainability

--- TalkCorner.API/Controllers/ThreadController.cs
+++ TalkCorner.API/Controllers/ThreadController.cs
@@ -1,10 +1,14 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using TalkCorner.API.Models;
+using TalkCorner.Application.Features.Thread.CloseThread;
 using TalkCorner.Application.Features.Thread.CreateThread;
 using TalkCorner.Application.Features.Thread.DeleteThread;
 using TalkCorner.Application.Features.Thread.GetThreadById;
 using TalkCorner.Application.Features.Thread.GetThreadsByBoardId;
+using TalkCorner.Application.Features.Thread.OpenThread;
+using TalkCorner.Application.Features.Thread.StickThread;
+using TalkCorner.Application.Features.Thread.UnstickThread;
 using TalkCorner.Application.Features.Thread.UpdateThread;
 
 namespace TalkCorner.API.Controllers;
@@ -14,6 +18,16 @@ namespace TalkCorner.API.Controllers;
 [ProducesErrorResponseType(typeof(CustomProblemDetails))]
 public class ThreadController(IMediator mediator) : ControllerBase
 {
+    [HttpGet("{id:guid}")]
+    [ProducesResponseType(typeof(GetThreadByIdDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<GetThreadByIdDto>> GetThreadByIdAsync(Guid id)
+    {
+        var request = new GetThreadByIdQuery { Id = id };
+        var response = await mediator.Send(request);
+        return Ok(response);
+    }
+
     [HttpGet("board/{boardId:guid}")]
     [ProducesResponseType(typeof(IEnumerable<GetThreadsByBoardIdDto>), StatusCodes.Status200OK)]
     public async Task<ActionResult<IEnumerable<GetThreadsByBoardIdDto>>> GetThreadsByBoardIdAsync(Guid boardId)
@@ -23,14 +37,14 @@ public class ThreadController(IMediator mediator) : ControllerBase
         return Ok(response);
     }
 
-    [HttpGet("{id:guid}")]
-    [ProducesResponseType(typeof(GetThreadByIdDto), StatusCodes.Status200OK)]
+    [HttpPost("close/{id:guid}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<ActionResult<GetThreadByIdDto>> GetThreadByIdAsync(Guid id)
+    public async Task<IActionResult> CloseThreadAsync(Guid id)
     {
-        var request = new GetThreadByIdQuery { Id = id };
-        var response = await mediator.Send(request);
-        return Ok(response);
+        var request = new CloseThreadCommand { Id = id };
+        await mediator.Send(request);
+        return NoContent();
     }
 
     [HttpPost]
@@ -42,6 +56,46 @@ public class ThreadController(IMediator mediator) : ControllerBase
         return StatusCode(StatusCodes.Status201Created);
     }
 
+    [HttpDelete("{id:guid}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> DeleteThreadAsync(Guid id)
+    {
+        var request = new DeleteThreadCommand { Id = id };
+        await mediator.Send(request);
+        return NoContent();
+    }
+
+    [HttpPost("open/{id:guid}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> OpenThreadAsync(Guid id)
+    {
+        var request = new OpenThreadCommand { Id = id };
+        await mediator.Send(request);
+        return NoContent();
+    }
+
+    [HttpPost("stick/{id:guid}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> StickThreadAsync(Guid id)
+    {
+        var request = new StickThreadCommand { Id = id };
+        await mediator.Send(request);
+        return NoContent();
+    }
+
+    [HttpPost("unstick/{id:guid}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> UnstickThreadAsync(Guid id)
+    {
+        var request = new UnstickThreadCommand { Id = id };
+        await mediator.Send(request);
+        return NoContent();
+    }
+
     [HttpPut("{id:guid}")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -49,16 +103,6 @@ public class ThreadController(IMediator mediator) : ControllerBase
     public async Task<IActionResult> UpdateThreadAsync(Guid id, [FromBody] UpdateThreadCommand request)
     {
         request.Id = id;
-        await mediator.Send(request);
-        return NoContent();
-    }
-
-    [HttpDelete("{id:guid}")]
-    [ProducesResponseType(StatusCodes.Status204NoContent)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<IActionResult> DeleteThreadAsync(Guid id)
-    {
-        var request = new DeleteThreadCommand { Id = id };
         await mediator.Send(request);
         return NoContent();
     }
